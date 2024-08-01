91cc36e03c857d0ce838af9a81d0caecc73e3961
Somdutta-git <choubeysomdutta@gmail.com>
2024-08-01 22:05:39 +0530

    Added Dbcontext with server and added besic api in the controller

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // /api/users
+    public class UserController(DataContext context) : ControllerBase
+    {
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = context.Users.ToList();
+            return users;
+        }
+
+        [HttpGet("{Id:int}")] // api/users/3
+        public ActionResult<AppUser> GetUser(int Id) 
+        {
+            var user = context.Users.Find(Id);
+
+            if (user == null) return NotFound();
+            
+            return user;
+        }
+    }
+}
